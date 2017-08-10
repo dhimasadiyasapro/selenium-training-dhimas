91bc868515b95e1b4a5faaff0dfd73fcc6a4284e
Dhimas <dhimas.adiyasa@gmail.com>
2017-08-10 22:28:45 +0700

    Update PR 4 Agustus 2017

--- features/step_definitions/check_get_business_category_step.rb
+++ features/step_definitions/check_get_business_category_step.rb
@@ -1,14 +1,29 @@
 When(/^I try to check business categories$/) do
-  x = get_business_types
-  puts x[0]["name"]
+  @x = get_business_types
+  z = 1
+  @x.each do
+    if !@x[z].nil?
+      puts @x[z]["name"]
+      z = z+1
+    end
+  end
 end
 
 Then(/^I should get response list of business categories by id$/) do
   y = 1
-  get_business_categories(1).each do
-    if !get_business_categories(1)[y].nil?
-      puts get_business_categories(1)[y]["name"]
-      y = y+1
+  a = 1
+  @x.each do
+    if !@x[a].nil?
+      puts @x[a]["name"]+" has following categories: "
+      get_business_categories(a).each do
+        if !get_business_categories(a)[y].nil?
+          puts "- "+get_business_categories(a)[y]["name"]
+          y = y+1
+        end
+      end
+      y = 1
+      a = a+1
+      puts " "
     end
   end
 end
